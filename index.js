184cfe7d6eb56b54bdcab8dbc56478cfc9caf942
Aaron Feinberg <aaron.feinberg@wework.com>
2019-01-22 00:07:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,12 +17,11 @@ logShout("string")
 function sayHitoGrandma(string) {
   var uppercase = "HELLO"
   var lowercase = "hello"
-}
-  if (string === string.toLowerCase()) {
+  if (string === string.toLowerCase()) 
     return "I can't hear you!"
-  }else if (string === string.toUpperCase()) {
+  else if (string === string.toUpperCase()) 
     return "YES INDEED!"
-  }else if (string === "I love you, Grandma.") {	  
+  else if (string === "I love you, Grandma.")  
     return "I love you, too."
   }
 
