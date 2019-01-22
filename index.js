24441d2dcea0f4a9a6a24882e48d66a443ec257c
Aaron Feinberg <aaron.feinberg@wework.com>
2019-01-22 00:02:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,10 +20,9 @@ function sayHitoGrandma(string) {
 }
   if (string === lowercase) {
     return "I can't hear you!"
-  else if (string === uppercase) {
+  }else if (string === uppercase) {
     return "YES INDEED!"
-} 
-  else if (string === "I love you, Grandma.") {	  
+  }else if (string === "I love you, Grandma.") {	  
     return "I love you, too."
   }
 
